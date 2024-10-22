783556d9984b200cefa3ccb389cd50878a5bb118
Seabass319 <bryanseabass319@gmail.com>
2024-10-22 17:09:29 -0600

    sdty

---
+++ leapyear.py
@@ -0,0 +1,12 @@
+year = int(input("Enter a year: "))
+
+div4 = year % 4
+div100 = year % 100
+div400 = year % 400
+
+if(div4 == 0 and div100 != 0):
+    print(f"{year} is a leap year")
+elif(div4 == 0 and div100 == 0 and div400 == 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
