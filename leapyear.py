adb27fbca9c15b7a778b1d0300f5327d54ccf786
Seabass319 <bryanseabass319@gmail.com>
2024-10-27 18:36:49 -0600

    sdty

--- leapyear.py
+++ leapyear.py
@@ -4,9 +4,9 @@ div4 = year % 4
 div100 = year % 100
 div400 = year % 400
 
-if(div4 == 0 and div100 != 0):
+if div4 == 0 and div100 != 0:
     print(f"{year} is a leap year")
-elif(div4 == 0 and div100 == 0 and div400 == 0):
+elif div4 == 0 and div100 == 0 and div400 == 0:
     print(f"{year} is a leap year")
 else:
     print(f"{year} is not a leap year")
